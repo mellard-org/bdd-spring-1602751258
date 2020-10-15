9d2601c160536e7683a5d368a66f6031006a11a4
Chris Mellard <chrism@datacom.co.nz>
2020-10-15 21:45:41 +1300

    Initial import

---
+++ src/test/java/com/example/bddspring1602751258/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bddspring1602751258;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
